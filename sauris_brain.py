406370da0b5fa0b42481610807293076832b914b
An0rak <phos4es@gmail.com>
2016-10-05 09:58:39 -0400

    added some basic get functions

--- sauris_brain.py
+++ sauris_brain.py
@@ -1,5 +1,22 @@
 import sauris_ears
 import sauris_mouth
+from datetime import datetime
+from __future__ import print_function
+import httplib2
+import os
+
+from apiclient import discovery
+from oauth2client import client
+from oauth2client import tools
+from oauth2client.file import Storage
+
+import datetime
+
+try:
+    import argparse
+    flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
+except ImportError:
+    flags = None
 
 #create ear
 ear = sauris_ears.ear('sauris_ear')
@@ -9,6 +26,12 @@ ear.create_listener()
 mouth = sauris_mouth.mouth('sauris_mouth')
 mouth.create_mouth()
 
+#variables
+SCOPES = 'https://www.googleapis.com/auth/calendar.readonly'
+CLIENT_SECRET_FILE = 'client_secret.json'
+APPLICATION_NAME = 'Google Calendar API Python Quickstart'
+
+
 def get_input(ear):
     text = ear.listen()
     return text
@@ -16,6 +39,60 @@ def get_input(ear):
 def speak(text):
     mouth.speak(text)
 
+def get_time():
+    time = datetime.now().time()
+    return time
+
+def get_date():
+    date = datetime.now().date()
+    return date
+
+def get_calender_creds():
+     home_dir = os.path.expanduser('~')
+    credential_dir = os.path.join(home_dir, '.credentials')
+    if not os.path.exists(credential_dir):
+        os.makedirs(credential_dir)
+    credential_path = os.path.join(credential_dir,
+                                   'calendar-python-quickstart.json')
+
+    store = Storage(credential_path)
+    credentials = store.get()
+    if not credentials or credentials.invalid:
+        flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
+        flow.user_agent = APPLICATION_NAME
+        if flags:
+            credentials = tools.run_flow(flow, store, flags)
+        else: # Needed only for compatibility with Python 2.6
+            credentials = tools.run(flow, store)
+        print('Storing credentials to ' + credential_path)
+    return credentials
+
+def get_events():
+    credentials = get_credentials()
+    http = credentials.authorize(httplib2.Http())
+    service = discovery.build('calendar', 'v3', http=http)
+
+    now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
+    print('Getting the upcoming 10 events')
+    eventsResult = service.events().list(
+        calendarId='primary', timeMin=now, maxResults=10, singleEvents=True,
+        orderBy='startTime').execute()
+    events = eventsResult.get('items', [])
+
+    if not events:
+        return('No upcoming events found.')
+    for event in events:
+        start = event['start'].get('dateTime', event['start'].get('date'))
+        return(start, event['summary'])
+
+def create_calender_event():
+
+
+def check_input(input_text):
+    for x in range 5:
+        if input_text != #sufhuifnsldf
+
 def main():
-    input_text=get_input(ear)
-    speak(input_text)
+    input_text
+    while input_text != 'sleep':
+        input_text=get_input(ear)
